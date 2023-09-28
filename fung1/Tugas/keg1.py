deb2d7fd6b0413ad6858cf0d537b321e3be871ab
Hazarddrips <123867164+Hazarddrips@users.noreply.github.com>
2023-09-28 15:02:00 +0700

    Update keg1.py

--- fung1/Tugas/keg1.py
+++ fung1/Tugas/keg1.py
@@ -14,7 +14,6 @@ def div(a, b):
 
 def tree(expression):
     if isinstance(expression, tuple):
-        # Pohon ekspresi adalah tupel
         left = tree(expression[0])
         operator = expression[1]
         right = tree(expression[2])
@@ -28,13 +27,10 @@ def tree(expression):
         elif operator == '/':
             return div(left, right)
     else:
-        # Pohon ekspresi adalah angka (basis rekursi)
         return expression
 
-# Ekspresi pohon yang diberikan
 expression_tree = ((2, '+', 3), '*', (5, '-', 1))
 
-# Mengevaluasi pohon ekspresi
 result = tree(expression_tree)
 
 print("Hasil evaluasi pohon ekspresi:", result)
